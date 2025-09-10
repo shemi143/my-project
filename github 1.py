8311909d433347830a5c7dfea4a5f1546f2b5c6d
shemi143 <shemeema0618@gmail.com>
2025-09-10 19:12:07 +0530

    Add files via upload
    Arithmetic operations

---
+++ github 1.py
@@ -0,0 +1,10 @@
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+sum=a+b
+print("sum of two numbers:",sum)
+difference=a-b
+print("difference of two numbers:",difference)
+product=a*b
+print("product of two numbers:",product)
+quotient=a/b
+print("quotient of two numbers:",quotient)
